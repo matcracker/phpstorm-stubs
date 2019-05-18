0ebadc07044e6d331c11da48d70227a3adf162a8
Dylan K. Taylor <odigiman@gmail.com>
2019-05-18 14:50:26 +0100

    added extra constants needed for leveldb-mcpe

--- LevelDB/LevelDB.php
+++ LevelDB/LevelDB.php
@@ -1,9 +1,13 @@
 <?php
 
-
 define("LEVELDB_NO_COMPRESSION", 0);
 
+/** May not be available */
 define("LEVELDB_SNAPPY_COMPRESSION", 1);
+
+define("LEVELDB_ZLIB_COMPRESSION", 2);
+
+define("LEVELDB_ZLIB_RAW_COMPRESSION", 4);
 
 
 class LevelDB{
