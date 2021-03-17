ca09adc6003721a4933be572d873becdcfa657ed
Dylan K. Taylor <dktapps@pmmp.io>
2021-03-17 21:15:24 +0000

    Updated modified stubs to match Jetbrains code style this makes it less painful to merge upstream changes.

--- morton/morton.php
+++ morton/morton.php
@@ -4,13 +4,13 @@
  * Creates a Morton code using the 32 least significant bits of two components
  * Note: Uniqueness is only guaranteed when both components are in the range -2147483648 ... 2147483657
  */
-function morton2d_encode(int $x, int $y) : int{}
+function morton2d_encode(int $x, int $y): int {}
 
 /**
  * Creates a Morton code using the 21 least significant bits of three components
  * Note: Uniqueness is only guaranteed when both components are in the range -1048576 ... 1048575
  */
-function morton3d_encode(int $x, int $y, int $z) : int{}
+function morton3d_encode(int $x, int $y, int $z): int {}
 
 /**
  * Decodes a 2D Morton code into its 32-bit components, including sign bits.
@@ -20,7 +20,7 @@ function morton3d_encode(int $x, int $y, int $z) : int{}
  * @return int[]
  * @phpstan-return array{int, int}
  */
-function morton2d_decode(int $morton) : array{}
+function morton2d_decode(int $morton): array {}
 
 /**
  * Decodes a 3D Morton code into its 21-bit components, including sign bits.
@@ -30,4 +30,4 @@ function morton2d_decode(int $morton) : array{}
  * @return int[]
  * @phpstan-return array{int, int, int}
  */
-function morton3d_decode(int $morton) : array{}
+function morton3d_decode(int $morton): array {}
